d023653fe1bbe43b54a01e3e4dd20d088f22c424
Daniel Cascales <daniel.cascales@lifullconnect.com>
2022-03-05 12:01:40 +0100

    wip

--- sshd_telegram_alert/requester.py
+++ sshd_telegram_alert/requester.py
@@ -19,7 +19,12 @@ class Requester():
         """
             Detect if there is an sshd opened session
         """
-        pam_type = os.environ()
+        pam_type = os.environ.get('PAM_TYPE')
+        pam_user = os.environ.get('PAM_USER')
+        pam_rhost = os.environ.get('PAM_RHOST')
+        
+        if pam_type == "open_session":
+            return True
 
     def send_message(self, config_path) -> None:
         """
@@ -29,11 +34,13 @@ class Requester():
         telegram_token = credentials["TELEGRAM_TOKEN"]
         chat_id = credentials["CHAT_ID"]
         base_url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
-        data = {'chat_id': chat_id, 'text': 'Testing'}
-        r = requests.post(url=base_url, data=data)
-        print(r.text)
 
-        if r.status_code == 200:
-            self.log.info("Message sended")
-        else:
-            self.log.error("Error sending message")
+        if self.open_session():
+            data = {'chat_id': chat_id, 'text': 'Testing'}
+            r = requests.post(url=base_url, data=data)
+            print(r.text)
+
+            if r.status_code == 200:
+                self.log.info("Message sended")
+            else:
+                self.log.error("Error sending message")
